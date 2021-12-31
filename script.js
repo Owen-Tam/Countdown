dfa85dc0edf33f48cd70f76f6cbb39815d497c13
WarriorX623 <owenhwtam@gmail.com>
2022-01-01 00:30:52 +0800

    fixed mins issue

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const timerText = document.querySelector(".timer");
 
-const newYear = new Date("January 1, 2022");
+const newYear = new Date("January 2, 2022");
 const numHours = document.querySelector(".num-hours");
 const numMins = document.querySelector(".num-mins");
 const numSecs = document.querySelector(".num-secs");
@@ -9,8 +9,10 @@ const tick = function () {
   const now = new Date();
   if (newYear - now > 0) {
     const hours = Math.trunc(Math.abs((newYear - now) / 1000 / 60 / 60));
-    const mins = Math.trunc(Math.abs((newYear - now) / 1000 / 60));
-    const secs = Math.trunc(Math.abs((newYear - now) / 1000 - mins * 60));
+    const mins = Math.trunc(Math.abs((newYear - now) / 1000 / 60) - hours * 60);
+    const secs = Math.trunc(
+      Math.abs((newYear - now) / 1000 - hours * 60 * 60 - mins * 60)
+    );
     numHours.textContent = hours;
     numMins.textContent = mins;
     numSecs.textContent = secs;
