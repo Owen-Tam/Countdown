07bc57a74df9e8d691f173e706dae989196dd5e2
WarriorX623 <owenhwtam@gmail.com>
2022-01-01 11:27:09 +0800

    2023 new year and frosted glass

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 const timerText = document.querySelector(".timer");
 
-const newYear = new Date("January 1, 2022");
+const newYear = new Date("January 1, 2023");
+const numDays = document.querySelector(".num-days");
 const numHours = document.querySelector(".num-hours");
 const numMins = document.querySelector(".num-mins");
 const numSecs = document.querySelector(".num-secs");
 
+// const tick = function () {
+//   const now = new Date();
+//   if (newYear - now > 0) {
+//     const hours = Math.trunc(Math.abs((newYear - now) / 1000 / 60 / 60));
+//     const mins = Math.trunc(Math.abs((newYear - now) / 1000 / 60) - hours * 60);
+//     const secs = Math.trunc(
+//       Math.abs((newYear - now) / 1000 - hours * 60 * 60 - mins * 60)
+//     );
+//     numHours.textContent = hours;
+//     numMins.textContent = mins;
+//     numSecs.textContent = secs;
+//   } else {
+//     timerText.textContent = "Timer has ended; Happy New Year!";
+//   }
+// };
 const tick = function () {
-  const now = new Date();
-  if (newYear - now > 0) {
-    const hours = Math.trunc(Math.abs((newYear - now) / 1000 / 60 / 60));
-    const mins = Math.trunc(Math.abs((newYear - now) / 1000 / 60) - hours * 60);
-    const secs = Math.trunc(
-      Math.abs((newYear - now) / 1000 - hours * 60 * 60 - mins * 60)
-    );
-    numHours.textContent = hours;
-    numMins.textContent = mins;
-    numSecs.textContent = secs;
-  } else {
-    timerText.textContent = "Timer has ended; Happy New Year!";
-  }
-};
-/*const tick = function () {
   const now = new Date();
   if (newYear - now > 0) {
     const days = Math.trunc(Math.abs((newYear - now) / 1000 / 60 / 60 / 24));
@@ -28,16 +29,22 @@ const tick = function () {
       Math.abs((newYear - now) / 1000 / 60 / 60) - days * 24
     );
     const mins = Math.trunc(
-      Math.abs((newYear - now) / 1000 / 60) - days * 24 - hours * 60
+      Math.abs((newYear - now) / 1000 / 60) - days * 24 * 60 - hours * 60
     );
     const secs = Math.trunc(
-      Math.abs((newYear - now) / 1000 - days * 24 - hours * 60 * 60 - mins * 60)
+      Math.abs(
+        (newYear - now) / 1000 -
+          days * 24 * 60 * 60 -
+          hours * 60 * 60 -
+          mins * 60
+      )
     );
+    numDays.textContent = days;
     numHours.textContent = hours;
     numMins.textContent = mins;
     numSecs.textContent = secs;
   } else {
     timerText.textContent = "Timer has ended; Happy New Year!";
   }
-}; */
+};
 setInterval(tick, 1000);
